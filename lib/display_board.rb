ad641ac3d3d9412e38a10ba14dce081539cff2d3
Serena Nakatani-Brown <serena_1994@hotmail.co.uk>
2018-09-02 15:36:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+98iboard = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
