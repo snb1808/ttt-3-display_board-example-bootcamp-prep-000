b7405170043bac6764b7312dff8c88c77e3b9e70
Serena Nakatani-Brown <serena_1994@hotmail.co.uk>
2018-09-02 16:39:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board
-  print "#{board[0]}|#{board[1]}|#{board[2]}"
-  print "-----------"
-  print "#{board[3]}|#{board[4]}|#{board[5]}"
-  print "-----------"
-  print "#{board[6]}|#{board[7]}|#{board[8]}"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
-display_board
+#display_board
